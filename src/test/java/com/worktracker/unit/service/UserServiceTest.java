6f7147114890bddf748fe9cc0a260fb9753329fc
Ivka Dimitrova <ivka.mdimitrova@gmail.com>
2020-10-28 20:23:32 +0200

    Remove redundant line

--- src/test/java/com/worktracker/unit/service/UserServiceTest.java
+++ src/test/java/com/worktracker/unit/service/UserServiceTest.java
@@ -35,55 +35,44 @@ public class UserServiceTest {
 
     @Test
     public void deleteUserSuccess() {
-        //when
         userService.deleteUser(1L);
 
-        //then
         verify(userRepository, times(1)).deleteById(1L);
     }
 
     @Test
     public void getEmployeesSuccess() {
-        //given
         User user = new User();
         user.setName("Test");
         when(userRepository.findAllByUserType(UserType.EMPLOYEE)).thenReturn(List.of(user));
 
-        //when
         List<User> result = userService.getEmployees();
 
-        //then
         assertEquals(1, result.size());
         assertEquals("Test", result.get(0).getName());
     }
 
     @Test
     public void getUserExist() {
-        //given
         User user = new User();
         user.setName("Test");
         when(userRepository.findById(1L)).thenReturn(Optional.of(user));
 
-        //when
         User result = userService.getUser(1L);
 
-        //then
         assertNotNull(result);
         assertEquals("Test", result.getName());
     }
 
     @Test(expected = WorktrackerException.class)
     public void getUserNonExist() {
-        //given
         when(userRepository.findById(1L)).thenReturn(Optional.empty());
 
-        //when
         userService.getUser(1L);
     }
 
     @Test
     public void createUserSuccess() {
-        //given
         UserRequestDTO userRequestDTO = new UserRequestDTO();
         userRequestDTO.setName("Test name");
         userRequestDTO.setEmail("Test email");
@@ -91,10 +80,8 @@ public class UserServiceTest {
 
         when(userRepository.save(any())).thenAnswer(i -> i.getArguments()[0]);
 
-        //when
         User result = userService.createUser(userRequestDTO);
 
-        //then
         assertNotNull(result);
         assertEquals("Test name", result.getName());
         assertEquals("Test email", result.getEmail());
